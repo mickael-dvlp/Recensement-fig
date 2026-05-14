d1217603b02960e9dcdce19b689952777ad575df
Mickael <mickael.martone@hotmail.fr>
2026-05-14 21:50:28 +0200

    maj

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* Options de configuration Next.js */
   images: {
-    // Domaines autorisés pour les images externes (à compléter si besoin)
     remotePatterns: [],
-    // Permet l'utilisation d'images SVG locales
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
   },
 };
 
